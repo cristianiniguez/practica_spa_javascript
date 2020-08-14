b1b59f35d6ee102cd47998072683e67d31934f0b
Cristian Felix Iñiguez Porcel <felizpc1@gmail.com>
2020-08-13 21:45:52 -0400

    version 1

---
+++ src/controllers/posts.controller.js
@@ -0,0 +1,27 @@
+import views from '../views/posts.html';
+
+const getPosts = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    return await response.json()
+}
+
+export default async () => {
+    const divElement = document.createElement('div')
+    divElement.innerHTML = views
+
+    const postsElement = divElement.querySelector('#posts')
+    const totalPosts = divElement.querySelector('#total')
+
+    const posts = await getPosts()
+    totalPosts.innerHTML = posts.length
+    posts.forEach(post => {
+        postsElement.innerHTML += `
+            <li class="list-group-item border-primary bg-dark text-white">
+                <h5>${post.title}</h5>
+                <p>${post.body}</p>
+            </li>
+        `
+    });
+
+    return divElement
+}
